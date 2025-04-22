de9a6f3a4e3447c8fb88efcc5f98ef3f15df5f3b
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-04-22 09:50:23 +0900

    Create BattleResult.cs

---
+++ team17_textRPG/team17_textRPG/BattleResult.cs
@@ -0,0 +1,69 @@
+﻿namespace team17_textRPG
+{
+    
+    // 전투 결과
+    public class BattleResult
+    {
+        public int ResultHp;
+        private static Character Character;
+
+        public int BattleHp = Character.Hp;
+        
+        // 이기면 빅토리 뜨고 체력 띄우기
+        void Victory()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Battle!! - Result");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nVictory");
+            Console.ResetColor();
+
+            Console.WriteLine($"\n던전에서 몬스터 {KilledMonster}마리를 잡았습니다.");
+
+            Console.WriteLine("Lv. {0:D2} {1}", Character.Lv, Character.Name);
+            Console.WriteLine($"HP {BattleHp} -> {ResultHp}");
+
+            Console.WriteLine("0. 다음");
+            Console.Write(">> ");
+
+            int result = CheckInput(0, 0);
+
+            switch (result)
+            {
+                case 0:
+                    DisplayMainUI();
+                    break;
+            }
+            
+        }
+
+        
+        
+        // 지면 루즈 뜨고 체력 띄우기
+        static void Lose()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Battle!! - Result");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nYou Lose...");
+            Console.ResetColor();
+
+            Console.WriteLine("Lv. {0:D2} {1}", Character.Lv, Character.Name);
+            Console.WriteLine($"HP {BattleHp} -> {ResultHp}");
+
+            Console.WriteLine("0. 다음");
+            Console.Write(">> ");
+
+            int result = Program.CheckInput(0, 0);
+
+            switch (result)
+            {
+                case 0:
+                    DisplayMainUI();
+                    break;
+            }
+        }
+    }
+}
